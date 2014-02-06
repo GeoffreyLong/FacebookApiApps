dc9e42fadca5d03ef90ef22d6759961b72705187
charliewright <wrightcharlesh@gmail.com>
2014-02-05 21:01:25 -0500

    Changed the file structure around, added Facebook node support

--- client/lobby.js
+++ client/lobby.js
@@ -1,5 +1,10 @@
 Template.lobby.events({
-  'click #logout' : function () {
-   Meteor.logout();
-  }
+	'click #logout' : function () {
+		Meteor.logout();
+	},
+	'click #btn-user-data': function(e) {
+		        Meteor.call('getUserData', function(err, data) {
+			             $('#result').text(JSON.stringify(data, undefined, 4));
+		         });
+	    }
 });
