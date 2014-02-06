2a903f8143d33e70ee44d09c377594a817a3e0a7
GeoffreyLong <Geoffrey.Long@mail.mcgill.ca>
2014-02-05 21:18:15 -0500

    Fix spacing issues

--- client/lobby.js
+++ client/lobby.js
@@ -3,8 +3,8 @@ Template.lobby.events({
 		Meteor.logout();
 	},
 	'click #btn-user-data': function(e) {
-		        Meteor.call('getUserData', function(err, data) {
-			             $('#result').text(JSON.stringify(data, undefined, 4));
-		         });
-	    }
+		Meteor.call('getUserData', function(err, data) {
+			$('#result').text(JSON.stringify(data, undefined, 4));
+			});
+		}
 });
