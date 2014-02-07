32790bec21031d92d99fccfb5849345a6d998b34
GeoffreyLong <Geoffrey.Long@mail.mcgill.ca>
2014-02-06 22:19:41 -0500

    Fix the fql statement

--- server/facebook.js
+++ server/facebook.js
@@ -22,17 +22,20 @@ Facebook.prototype.query = function(query, method) {
 }
 
 Facebook.prototype.getUserData = function() {
-    return this.query('me/photos');
+    return this.query('/fql?q=SELECT%20src_big%2C%20caption%2C%20created%2C%20like_info%20FROM%20photo%20WHERE%20object_id%20IN%20(SELECT%20object_id%20FROM%20photo_tag%20WHERE%20subject%3Dme())%20LIMIT%2010&access_token=CAACZBSuuVIY4BAFNxPiyBsSim5O8tZCpK8HE9sgN0DAZBWK0qR6NhI7YDD0Ntr7B5xJUDeW7fpyHoqbdtIfqswQDrS8PQDSaRRwwpCx8VgBBtfE9pI4l9zzrck5AWKrlSCUpKdxhqNe3tFeDnGUHZCr0ZAHLBoyDEv1B5YD8a0s8RzroaUhyc');
 }
 
 Meteor.methods({
     getUserData: function() {
-    	var fb = new Facebook(Meteor.user().services.facebook.accessToken);
-    	var query = "SELECT src_big, caption, created, like_info FROM photo WHERE object_id IN (SELECT object_id FROM photo_tag WHERE subject=me()) LIMIT 10";
-    	var data = Meteor.require('fbgraph').fql(query, function (e, res){
+        var fb = new Facebook(Meteor.user().services.facebook.accessToken);
+        var data = fb.getUserData();
+        
+        var query = "SELECT src_big, caption, created, like_info FROM photo WHERE object_id IN (SELECT object_id FROM photo_tag WHERE subject=me()) LIMIT 10";
+    	var dataset = Meteor.require('fbgraph').fql(query, function (e, res){
     		return res;
     	});
-        //var data = fb.getUserData();
+    	console.log(dataset);
+    	
         return data;
     }
 });
