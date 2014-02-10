24fe55586e8d190efbe03516d1d8b1f371a23c52
GeoffreyLong <Geoffrey.Long@mail.mcgill.ca>
2014-02-10 01:25:51 -0500

    Framework of the multiquery call

--- client/lobby.js
+++ client/lobby.js
@@ -3,14 +3,18 @@ Template.lobby.events({
 		Meteor.logout();
 	},
 	'click #btn-user-data': function(e) {
-		Meteor.call('getUserData', function(err, data) {
-			var actual = data.data.data;
-			for(var i = 0; i < actual.length; i++ ) {
-				var newObj = $('"' + actual[i].embed_html + '"');
-				newObj.appendTo($('#result'));
+		Meteor.call('getFriendCount', function(errOne, data) {
+			for (var j = 0; j< data.data.data[0].friend_count; j+=50){
+				var next = parseInt(j) +50;
+				Meteor.call('getVideos(' + parseInt(j) + ', ' + next + ')', function(errTwo, videos) {
+					var actual = data.data.data;
+					for(var i = 0; i < actual.length; i++ ) {
+						var newObj = $('"' + actual[i].embed_html + '"');
+						newObj.appendTo($('#result'));
+					}
+				});
 			}
 		});
-		
-	}
+	},
 		
 });
--- server/facebook.js
+++ server/facebook.js
@@ -26,9 +26,16 @@ Facebook.prototype.getUserData = function() {
 } */ 
 
 Meteor.methods({
-    getUserData: function() {
-        var token = Meteor.user().services.facebook.accessToken;
-        var query = ("q=SELECT embed_html FROM video WHERE owner IN (SELECT uid2 FROM friend WHERE uid1 = me() LIMIT 100) AND (strpos(lower(description),lower('neknomination')) >=0 OR strpos(lower(description),lower('neknom')) >=0 OR strpos(lower(description),lower('necknom')) >=0)");
+	getFriendCount: function () {
+		var token = Meteor.user().services.facebook.accessToken;
+        var query = ("q=SELECT friend_count FROM user WHERE uid = me()");
+        var fb_url = 'https://graph.facebook.com';
+        var path = fb_url + '/fql?' + encodeURI(query) + '&method=GET&metadata=true&format=json&access_token=' + token;
+        return (Meteor.http.get(path));
+	},
+	getVideos: function(startNum, endNum) {
+    	var token = Meteor.user().services.facebook.accessToken;
+        var query = ("q=SELECT embed_html FROM video WHERE owner IN (SELECT uid2 FROM friend WHERE uid1 = me() LIMIT "+ startNum + " OFFSET " + endNum + ") AND (strpos(lower(description),lower('neknomination')) >=0 OR strpos(lower(description),lower('neknom')) >=0 OR strpos(lower(description),lower('necknom')) >=0)");
         var fb_url = 'https://graph.facebook.com';
         var path = fb_url + '/fql?' + encodeURI(query) + '&method=GET&metadata=true&format=json&access_token=' + token;
         return (Meteor.http.get(path));
